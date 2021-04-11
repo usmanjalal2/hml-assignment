c068021d21614d40b81e1a6806fbf6e7fb7ce8eb
usmanjalal <78979322+usmanjalal2@users.noreply.github.com>
2021-04-10 23:20:57 -1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+//Counts the number of vowels within a string
+
+function vowel_count(str1)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vcount = 0;
+  
+  for(var x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+  return vcount;
+}
+console.log(vowel_count("Counts the number of vowels within a string"));
+
+
+
+//Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case.
+
+function uppercase(str)
+{
+  var array1 = str.split(' ');
+  var newarray1 = [];
+    
+  for(var x = 0; x < array1.length; x++){
+      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+  }
+  return newarray1.join(' ');
+}
+console.log(uppercase("rite a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case"));
+
+
+//JavaScript Data Types
+function detect_data_type(value)
+{
+var dtypes = [Function, RegExp, Number, String, Boolean, Object], x, len;
+    
+if (typeof value === "object" || typeof value === "function") 
+    {
+     for (x = 0, len = dtypes.length; x < len; x++) 
+     {
+            if (value instanceof dtypes[x])
+            {
+                return dtypes[x];
+            }
+      }
+    }
+    
+    return typeof value;
+}
+console.log(detect_data_type('abdul'));
+console.log(detect_data_type(8));
+console.log(detect_data_type(false));
+
+
+
+//The area of a triangle</
+var side1 = 5; 
+var side2 = 6; 
+var side3 = 7; 
+var s = (side1 + side2 + side3)/2;
+var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+console.log(area);
+
+
+
+
+
+
+
+
+
+
